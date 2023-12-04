833f00e2445636f8c79c95b87285549062f0ea2c
ksk-461 <lpcaster@yahoo.co.jp>
2023-12-05 02:18:36 +0900

    ベース作成

---
+++ post.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>post</title>
+</head>
+<body>
+    post
+    <a href="./write.php">write</a>
+</body>
+</html>
---
+++ write.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>write</title>
+</head>
+<body>
+    write
+    <a href="./post.php">post</a>
+    <a href="./read.php">read</a>
+</body>
+</html>
