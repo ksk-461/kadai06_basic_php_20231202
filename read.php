9071bea794851ac6ba744b92878967b11e7e7799
ksk-461 <lpcaster@yahoo.co.jp>
2023-12-08 03:07:26 +0900

    一時提出

--- read.php
+++ read.php
@@ -1,6 +1,10 @@
 <?php
     $cul = file('data.txt');
-    print_r($cul);
+    // print_r($cul);
+    $length = count($cul);
+    $data = json_decode($cul[1]);
+    print_r($data);
+    echo $data->name;
 ?>
 
 <!DOCTYPE html>
@@ -9,9 +13,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
-    <title>complete</title>
+    <title>read</title>
 </head>
 <body>
     
+    <script src="./js/read.js"></script>
 </body>
 </html>
