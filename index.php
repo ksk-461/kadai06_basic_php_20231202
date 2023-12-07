97573f0114e7ae85415164f2ebe75ae5b34f2e81
ksk-461 <lpcaster@yahoo.co.jp>
2023-12-08 00:02:42 +0900

    日付の暫定値設定

--- index.php
+++ index.php
@@ -19,49 +19,49 @@
                 <div>
                     <div class="name"><input type="text" class="form" name="name" placeholder="name"></div>
                     <div class="mail"><input type="text" class="form" name="mail" placeholder="mail"></div>
-                    <div><input type="date" id="sunday" class="form" name="date" value="2023-11-26"></div>
+                    <!-- <div><input type="date" id="sunday" class="form" name="date" value="2023-11-26"></div> -->
                 </div>
                 <div class="time">
                     Sun.
-                    <input type="datetime-local" class="form st" name="sun_st" value="2023-12-01 00:00">
+                    <input type="datetime-local" class="form st" name="sun_st" value="2023-12-03 21:00">
                     ～
-                    <input type="datetime-local" class="form ed" name="sun_ed">
+                    <input type="datetime-local" class="form ed" name="sun_ed" value="2023-12-04 09:00">
                 </div>
                 <div class="time">
                     Mon.
-                    <input type="datetime-local" class="form st" name="mon_st">
+                    <input type="datetime-local" class="form st" name="mon_st" value="2023-12-04 21:00">
                     ～
-                    <input type="datetime-local" class="form ed" name="mon_ed">
+                    <input type="datetime-local" class="form ed" name="mon_ed" value="2023-12-05 09:00">
                 </div>
                 <div class="time">
                     Tue.
-                    <input type="datetime-local" class="form st" name="tue_st">
+                    <input type="datetime-local" class="form st" name="tue_st" value="2023-12-05 21:00">
                     ～
-                    <input type="datetime-local" class="form ed" name="tue_ed">
+                    <input type="datetime-local" class="form ed" name="tue_ed" value="2023-12-06 09:00">
                 </div>
                 <div class="time">
                     Wed.
-                    <input type="datetime-local" class="form st" name="wed_st">
+                    <input type="datetime-local" class="form st" name="wed_st" value="2023-12-06 21:00">
                     ～
-                    <input type="datetime-local" class="form ed" name="wed_ed">
+                    <input type="datetime-local" class="form ed" name="wed_ed" value="2023-12-07 09:00">
                 </div>
                 <div class="time">
                     Thu.
-                    <input type="datetime-local" class="form st" name="thu_st">
+                    <input type="datetime-local" class="form st" name="thu_st" value="2023-12-07 21:00">
                     ～
-                    <input type="datetime-local" class="form ed" name="thu_ed">
+                    <input type="datetime-local" class="form ed" name="thu_ed" value="2023-12-08 09:00">
                 </div>
                 <div class="time">
                     Fri. 
-                    <input type="datetime-local" class="form st" name="fri_st">
+                    <input type="datetime-local" class="form st" name="fri_st" value="2023-12-08 21:00">
                     ～
-                    <input type="datetime-local" class="form ed" name="fri_ed">
+                    <input type="datetime-local" class="form ed" name="fri_ed" value="2023-12-09 09:00">
                 </div>
                 <div class="time">
                     Sat.
-                    <input type="datetime-local" class="form st" name="sat_st">
+                    <input type="datetime-local" class="form st" name="sat_st" value="2023-12-09 21:00">
                     ～
-                    <input type="datetime-local" class="form ed" name="sat_ed">
+                    <input type="datetime-local" class="form ed" name="sat_ed" value="2023-12-10 09:00">
                 </div>
                 <input type="submit" class="submit" value="good night">
             </form>
