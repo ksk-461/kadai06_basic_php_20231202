ad6a00ccf00670014ac1fdf5706d8a7d539f04f7
ksk-461 <lpcaster@yahoo.co.jp>
2023-12-07 22:06:35 +0900

    index大まかに整え済み

--- index.php
+++ index.php
@@ -8,57 +8,63 @@
 </head>
 <body>
     <div class="wrap">
-        <h1>good night</h1>
+        <h1>How long did you sleep?</h1>
         <div id="clock2">
             <div id="hour-hand2"></div>
             <div id="minute-hand2"></div>
             <div id="second-hand2"></div>
         </div>
-        <form action="confirm.php" method="post">
-            <div class="time">
-                Sun.
-                <input type="time" class="form" name="sun_st" value="21:00">
-                ～
-                <input type="time" class="form" name="sun_ed" value="00:00">
-            </div>
-            <div class="time">
-                Mon.
-                <input type="time" class="form" name="mon_st" value="21:00">
-                ～
-                <input type="time" class="form" name="mon_ed" value="00:00">
-            </div>
-            <div class="time">
-                Tue.
-                <input type="time" class="form" name="tue_st" value="21:00">
-                ～
-                <input type="time" class="form" name="tue_ed" value="00:00">
-            </div>
-            <div class="time">
-                Wed.
-                <input type="time" class="form" name="wed_st" value="21:00">
-                ～
-                <input type="time" class="form" name="wed_ed" value="00:00">
-            </div>
-            <div class="time">
-                Thu.
-                <input type="time" class="form" name="thu_st" value="21:00">
-                ～
-                <input type="time" class="form" name="thu_ed" value="00:00">
-            </div>
-            <div class="time">
-                Fri.
-                <input type="time" class="form" name="fri_st" value="21:00">
-                ～
-                <input type="time" class="form" name="fri_ed" value="00:00">
-            </div>
-            <div class="time">
-                Sat.
-                <input type="time" class="form" name="sat_st" value="21:00">
-                ～
-                <input type="time" class="form" name="sat_ed" value="00:00">
-            </div>
-            <input type="submit" class="submit" value="submit">
-        </form>
+        <div class="select">
+            <form action="confirm.php" method="post">
+                <div>
+                    <div class="name"><input type="text" class="form" name="name" placeholder="name"></div>
+                    <div><input type="text" class="form" name="mail" placeholder="mail"></div>
+                </div>
+                <div class="time">
+                    Sun.
+                    <input type="time" class="form" name="sun_st" value="22:00">
+                    ～
+                    <input type="time" class="form" name="sun_ed" value="07:00">
+                </div>
+                <div class="time">
+                    Mon.
+                    <input type="time" class="form" name="mon_st" value="22:00">
+                    ～
+                    <input type="time" class="form" name="mon_ed" value="07:00">
+                </div>
+                <div class="time">
+                    Tue.
+                    <input type="time" class="form" name="tue_st" value="22:00">
+                    ～
+                    <input type="time" class="form" name="tue_ed" value="07:00">
+                </div>
+                <div class="time">
+                    Wed.
+                    <input type="time" class="form" name="wed_st" value="22:00">
+                    ～
+                    <input type="time" class="form" name="wed_ed" value="07:00">
+                </div>
+                <div class="time">
+                    Thu.
+                    <input type="time" class="form" name="thu_st" value="22:00">
+                    ～
+                    <input type="time" class="form" name="thu_ed" value="07:00">
+                </div>
+                <div class="time">
+                    Fri. 
+                    <input type="time" class="form" name="fri_st" value="22:00">
+                    ～
+                    <input type="time" class="form" name="fri_ed" value="07:00">
+                </div>
+                <div class="time">
+                    Sat.
+                    <input type="time" class="form" name="sat_st" value="22:00">
+                    ～
+                    <input type="time" class="form" name="sat_ed" value="07:00">
+                </div>
+                <input type="submit" class="submit" value="good night">
+            </form>
+        </div>
     </div>
     <script src="./js/index.js"></script>
 </body>
